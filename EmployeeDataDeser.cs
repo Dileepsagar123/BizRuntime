1d93f004563baa925d9fd877e1138c8083ec1c24
Dileepsagar123 <dileepsagar1@gmail.com>
2018-09-04 09:52:32 +0530

    JSON

---
+++ EmployeeDataDeser.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONAPP
+{
+    class EmployeeDataDeser
+    {
+        public string ReadJsonFile(string JsonfilePath)
+
+        {
+
+            string json = string.Empty;
+
+            using (StreamReader r = new StreamReader("D://EmployeeData.json"))
+
+            {
+
+                json = r.ReadToEnd();
+
+                dynamic array = JsonConvert.DeserializeObject(json);
+
+                //... read text from json file
+
+            }
+
+            return json;
+
+        }
+        static void Main(string[] args)
+        {
+            string path = @"D:\EmployeeData.json";
+            EmployeeDataDeser employeeDataDeser = new EmployeeDataDeser();
+            Console.WriteLine(employeeDataDeser.ReadJsonFile(path));
+            Console.ReadLine();
+           
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+namespace JSONAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> lstemployee = new List<Employee>();
+            lstemployee.Add(new Employee()
+            {
+                EmployeeID = 100,
+                EmployeeName = "Pradeep",
+                DeptWorking = "OnLineBanking",
+                Salary = 10000
+            });
+            lstemployee.Add(new Employee()
+            {
+                EmployeeID = 101,
+                EmployeeName = "Mark",
+                DeptWorking = "OnLineBanking",
+                Salary = 20000
+            });
+            lstemployee.Add(new Employee()
+            {
+                EmployeeID = 102,
+                EmployeeName = "Smith",
+                DeptWorking = "Mobile banking",
+                Salary = 10000
+            });
+            lstemployee.Add(new Employee()
+            {
+                EmployeeID = 103,
+                EmployeeName = "John",
+                DeptWorking = "Testing",
+                Salary = 7000
+            });
+            lstemployee.Add(new Employee()
+            {
+                EmployeeID = 104,
+                EmployeeName = "Johny sins",
+                DeptWorking = "R & D",
+                Salary = 70000
+            });
+            lstemployee.Add(new Employee()
+            {
+                EmployeeID = 105,
+                EmployeeName = "Johny sins",
+                DeptWorking = "R & D",
+                Salary = 70000
+            });
+            string output = JsonConvert.SerializeObject(lstemployee.ToArray());
+            string path = @"EmployeeData.json";
+            //string deserPath = @"EmployeeDataDeser.json";
+            if (File.Exists(path))
+            {
+                File.CreateText(path);
+            }
+            else
+            {
+                File.WriteAllText(@"D:\EmployeeData.json", output);
+                
+            }
+            try
+            {
+                string json = string.Empty;
+
+                using (StreamReader r = new StreamReader("D://EmployeeData.json"))
+
+                {
+
+                    json = r.ReadToEnd();
+
+                    dynamic array = JsonConvert.DeserializeObject(json);
+                    //StreamWriter streamWriter = new StreamWriter(array);
+                    
+                    Console.WriteLine(array);
+
+                    //... read text from json file
+
+                }
+
+               // var employeeDeser = JsonConvert.DeserializeObject<Employee[]>(output);
+                    
+                 //   foreach (var frmt in employeeDeser)
+                   // {
+                     //   Console.WriteLine("Employee ID :  {0}",frmt.EmployeeID.ToString());
+                       // Console.WriteLine("Employee Salary : {0}", frmt.Salary.ToString());
+                        //Console.WriteLine("Employee Dept : {0}", frmt.DeptWorking.ToString());
+                        //Console.WriteLine("Employee Name : {0}", frmt.EmployeeName.ToString());
+                       
+                    //}
+                     
+                
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
